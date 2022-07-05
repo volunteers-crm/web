40dc969a2dfe131b50bd1e3314f48deb3a065262
Andrey Helldar <helldar@ai-rus.com>
2022-07-05 22:21:13 +0300

    Added redirect to the admin panel from the login page if the user is authorized

--- src/pages/auth/sign-in.vue
+++ src/pages/auth/sign-in.vue
@@ -11,9 +11,22 @@ import VPage from '@/components/pages/info.vue'
 import VRegister from '@/pages/auth/register.vue'
 import TelegramSignInBtn from '@/components/buttons/telegram/sign-in.vue'
 
-import {computed} from "vue";
+import {ROUTE_ADMIN_DASHBOARD} from "@/routes/names";
+
+import {computed, onBeforeMount} from "vue";
+import {useStore} from "vuex";
+import {useRouter} from "vue-router";
 
 import url from '@/plugins/url'
 
+const store = useStore()
+const router = useRouter()
+
 const isRegister = computed(() => url.hasParam('id'))
+
+onBeforeMount(() => {
+    if (store.getters['user/isLogged']) {
+        router.push({name: ROUTE_ADMIN_DASHBOARD})
+    }
+})
 </script>
