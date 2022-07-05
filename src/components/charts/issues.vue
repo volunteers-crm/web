b4ea33e7c5554d87a0f276147d5b08f3dc8f4b1e
Andrey Helldar <helldar@ai-rus.com>
2022-07-05 23:14:14 +0300

    Added curators chart

---
+++ src/components/charts/issues.vue
@@ -0,0 +1,88 @@
+<template>
+    <v-card>
+        <v-card-title>
+            {{ $t('Issues') }}
+        </v-card-title>
+
+        <v-card-text>
+            <apexchart
+                :options="issues.options"
+                :series="issues.series"
+                height="200"
+                type="area"
+            />
+
+            <div
+                class="text-right text-grey-darken-1"
+                v-text="$t('Updated every 15 minutes.')"
+            />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+import {trans} from "laravel-vue-i18n";
+
+import moment from "moment";
+
+// TODO: remove fake generator
+const count = 5;
+
+const randomData = () => {
+    const min = 10
+    const max = 90
+
+    let values = [];
+
+    for (let i = 0; i < count; i++) {
+        const value = Math.floor(Math.random() * (max - min + 1) + min)
+
+        values.push(value)
+    }
+
+    return values
+}
+
+const randomDates = () => {
+    let values = [];
+
+    for (let i = count; i > 0; i--) {
+        const date = moment().day(i * -1).format('MMMM, Do')
+
+        values.push(date)
+    }
+
+    return values;
+}
+
+const issues = ref({
+    options: {
+        chart: {
+            id: "chart-issues",
+        },
+        xaxis: {
+            categories: randomDates(),
+        },
+    },
+
+    series: [
+        {
+            name: trans('opened'),
+            data: randomData(),
+        },
+        {
+            name: trans('solved'),
+            data: randomData(),
+        },
+        {
+            name: trans('cancelled'),
+            data: randomData(),
+        },
+        {
+            name: trans('unassigned'),
+            data: randomData(),
+        }
+    ]
+})
+</script>
--- src/pages/manage/dashboard.vue
+++ src/pages/manage/dashboard.vue
@@ -1,85 +1,14 @@
 <template>
     <v-row>
         <v-col>
-            <v-card>
-                <v-card-title>
-                    {{ $t('Issues') }}
-                </v-card-title>
-
-                <v-card-text>
-                    <apexchart
-                        :options="issues.options"
-                        :series="issues.series"
-                        height="200"
-                        type="area"
-                    />
-
-                    <div
-                        class="text-right text-grey-darken-1"
-                        v-text="$t('Updated every 15 minutes.')"
-                    />
-                </v-card-text>
-            </v-card>
+            <issues-chart />
+        </v-col>
+        <v-col>
+            <curators-chart />
         </v-col>
     </v-row>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
-import {trans} from "laravel-vue-i18n";
-
-// TODO: remove fake generator
-const randomData = () => {
-    const min = 10
-    const max = 90
-
-    let values = [];
-
-    for (let i = 0; i < 8; i++) {
-        const value = Math.floor(Math.random() * (max - min + 1) + min)
-
-        values.push(value)
-    }
-
-    return values
-}
-
-const issues = ref({
-    options: {
-        chart: {
-            id: "vuechart-example",
-        },
-        xaxis: {
-            categories: [
-                '2022-07-05',
-                '2022-07-04',
-                '2022-07-03',
-                '2022-07-02',
-                '2022-07-01',
-                '2022-06-30',
-                '2022-06-29',
-                '2022-06-28',
-            ],
-        },
-    },
-
-    series: [
-        {
-            name: trans('opened'),
-            data: randomData(),
-        },
-        {
-            name: trans('solved'),
-            data: randomData(),
-        },
-        {
-            name: trans('cancelled'),
-            data: randomData(),
-        },
-        {
-            name: trans('unassigned'),
-            data: randomData(),
-        }
-    ]
-})
-</script>
+import IssuesChart from '@/components/charts/issues.vue'
+import CuratorsChart from '@/components/charts/curators.vue'</script>
