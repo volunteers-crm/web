f71a562e5799dd3d07a4c376d93e7996bd3b1452
Andrey Helldar <helldar@ai-rus.com>
2022-07-07 01:20:26 +0300

    Prepared to extend RouteMeta interface

---
+++ src/modules/vue-router.d.ts
@@ -0,0 +1,11 @@
+import 'vue-router'
+
+declare module 'vue-router' {
+    interface RouteMeta {
+        requiresAuth?: boolean,
+        layout?: string,
+        title: string,
+        icon?: string,
+        show?: boolean
+    }
+}
