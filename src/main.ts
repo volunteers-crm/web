562daea3c3e444121ffe4deb6d5f586a85aeadfe
Andrey Helldar <helldar@ai-rus.com>
2022-07-08 02:56:01 +0300

    Changed import dependencies order

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import pinia from '@/plugins/pinia'
+
 import { createApp } from 'vue'
 import { i18nVue } from 'laravel-vue-i18n'
 import VueApexCharts from 'vue3-apexcharts'
@@ -6,7 +8,6 @@ import Toast from 'vue-toastification'
 import ToastOptions from '@/plugins/toast'
 
 import routes from '@/routes'
-import pinia from '@/plugins/pinia'
 
 import vuetify from '@/plugins/vuetify'
 import i18nOptions from '@/plugins/lang'
