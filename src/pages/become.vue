d02dc1296adf45c0a8946b46119743efd96c635b
Andrey Helldar <helldar@ai-rus.com>
2022-07-18 13:09:55 +0300

    Updated `Become a coordinator` form

--- src/pages/become.vue
+++ src/pages/become.vue
@@ -4,50 +4,49 @@
             {{ $t('Become a coordinator') }}
         </h1>
 
-        <wizard
-            :custom-tabs="tabs"
-            navigable-tabs
-            @change="onChangeTab"
-        >
-            <div>
-                <div v-if="hasTab(0)">
+        <v-card elevation="0">
+            <v-card-text>
+                <p class="mb-2">
+                    {{ $t('Specify a link to the bot you want to become the coordinator of.') }}
+                </p>
+
+                <v-text-field
+                    v-model="form.bot"
+                    :hint="$t('For example, @VolunteersCrmBot or https://t.me/VolunteersCrmBot.')"
+                    :label="$t('Bot Username')"
+                    autofocus
+                    density="comfortable"
+                />
+            </v-card-text>
+
+            <v-card-actions class="justify-center">
+                <v-btn
+                    class="px-10"
+                    variant="elevates"
+                    @click="loadBot"
+                >
+                    {{ $t('Find a team') }}
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+
+        <v-fade-transition>
+            <v-card v-if="hasLoadedBot" elevation="0">
+                <v-card-text>
                     <v-row>
-                        <v-col class="text-h6 mb-2" cols="12">
-                            {{ $t('Specify a link to the bot you want to become the coordinator of.') }}
-                        </v-col>
-
-                        <v-col cols="12">
-                            {{ $t('For example, @VolunteersCrmBot or https://t.me/VolunteersCrmBot.') }}
-                        </v-col>
-
-                        <v-col cols="12">
-                            <v-text-field
-                                v-model="form.bot"
-                                :label="$t('Bot Username')"
-                                density="comfortable"
-                                variant="underlined"
-                            />
-                        </v-col>
-                    </v-row>
-                </div>
-
-                <div v-if="hasTab(1)">
-                    <v-row>
-                        <v-col cols="12">
+                        <v-col cols="12" sm="6">
                             <v-text-field
                                 v-model="form.name"
                                 :label="$t('How can I call you?')"
                                 density="comfortable"
-                                variant="underlined"
                             />
                         </v-col>
 
-                        <v-col cols="12">
+                        <v-col cols="12" sm="6">
                             <v-text-field
                                 v-model="form.city"
                                 :label="$t('What city do you live in?')"
                                 density="comfortable"
-                                variant="underlined"
                             />
                         </v-col>
 
@@ -61,29 +60,18 @@
                         <v-col cols="12">
                             <v-textarea
                                 v-model="form.about"
-                                :hint="$t('Please tell us about yourself! If you have experience volunteering or working in social projects, don\'t forget to mention!')"
-                                :label="$t('About you')"
-                                persistent-hint
+                                :label="$t('Please tell us about yourself! If you have experience volunteering or working in social projects, don\'t forget to mention!')"
+                                rows="3"
                             />
                         </v-col>
-                    </v-row>
-                </div>
 
-                <div v-if="hasTab(2)">
-                    <v-row>
                         <v-col cols="12">
                             <v-textarea
                                 v-model="form.source"
+                                :hint="$t('We carefully and attentively treat the information we work with, so it is very important for us to understand who the people who come to us are. If there are specific people who referred you, please refer to them.')"
                                 :label="$t('How did you hear about volunteering for the :name project?', { name: botName })"
+                                rows="3"
                             />
-                        </v-col>
-
-                        <v-col cols="12">
-                            {{ $t('We carefully and attentively treat the information we work with, so it is very important for us to understand who the people who come to us are.') }}
-                        </v-col>
-
-                        <v-col cols="12">
-                            {{ $t('If there are specific people who referred you, please refer to them.') }}
                         </v-col>
 
                         <v-col cols="12">
@@ -93,11 +81,7 @@
                                 :label="$t('Recommendations')"
                             />
                         </v-col>
-                    </v-row>
-                </div>
 
-                <div v-if="hasTab(3)">
-                    <v-row>
                         <v-col cols="12">
                             <v-select
                                 v-model="form.roles"
@@ -105,30 +89,22 @@
                             />
                         </v-col>
                     </v-row>
-                </div>
-            </div>
-        </wizard>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-btn>
+                        {{ $t('Sent') }}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-fade-transition>
     </v-container>
 </template>
 
 <script lang="ts" setup>
-import 'form-wizard-vue3/dist/form-wizard-vue3.css'
-
 import VTodo from '@/components/lists/todo.vue'
 
-// https://github.com/bahadirsofuoglu/form-wizard-vue3?ref=vuejsexamples.com
-import Wizard from 'form-wizard-vue3'
 import { computed, ref } from 'vue'
-import { trans } from 'laravel-vue-i18n'
-
-const tabIndex = ref(0)
-
-const tabs = ref([
-    { title: trans('Volunteer Team'), icon: 'back' },
-    { title: trans('About You'), icon: 'back' },
-    { title: trans('About Team'), icon: 'back' },
-    { title: trans('Team Roles'), icon: 'back' }
-])
 
 const form = ref({
     bot: null,
@@ -147,15 +123,10 @@ const bot = ref({
 
 const botName = computed(() => bot.value.name)
 
-const onChangeTab = (index: number) => {
-    tabIndex.value = index
-}
-
-const hasTab = (index: number): boolean => {
-    return tabIndex.value === index
-}
+const hasLoadedBot = computed(() => !! bot.value?.id)
 
 const loadBot = () => {
+    bot.value.id = 1
     bot.value.name = 'Some Bot'
 }
 </script>
