bf3529e8e2d23d16f318bff8e12d8e8373941818
Andrey Helldar <helldar@ai-rus.com>
2022-07-23 03:05:04 +0300

    Localizations folder moved to the root of the project

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -4,7 +4,7 @@ import { useSettingsStore } from '@/stores/settings'
 export default (pinia: Pinia) => {
     const lang = useSettingsStore(pinia).locale
 
-    const resolve = (lang: string) => import(`../lang/${ lang }.json`)
+    const resolve = (lang: string) => import(`../../lang/${ lang }.json`)
 
     return { lang, resolve }
 }
