e558bf8018eae1b1bf523ac3d36258e158f78644
Andrey Helldar <helldar@dragon-code.pro>
2022-08-23 02:36:48 +0300

    Cannot read properties of undefined (reading 'trim')

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,1 +1,1 @@
-export const APPLICATION_TITLE = import.meta.env?.VITE_APP_NAME.trim() || 'Volunteers CRM'
+export const APPLICATION_TITLE = import.meta.env?.VITE_APP_NAME?.trim() || 'Volunteers CRM'
