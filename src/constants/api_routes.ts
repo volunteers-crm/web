a4ecbe3f0779df0fe62ef2bad258e0c9b865bfd7
Andrey Helldar <helldar@dragon-code.pro>
2022-10-24 22:32:16 +0300

    Updated API Auth confirmation route

--- src/constants/api_routes.ts
+++ src/constants/api_routes.ts
@@ -1,4 +1,4 @@
-export const API_AUTH_CONFIRM = '/auth/telegram/confirm'
+export const API_AUTH_CONFIRM = '/auth/telegram'
 export const API_AUTH_ME = '/auth/me'
 
 export const API_URL_PAGE = '/pages/:slug'
