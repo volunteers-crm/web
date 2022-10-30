f2cfe6dadea4a6f08f40bfaf9b0f646c7fca2f0f
Andrey Helldar <helldar@dragon-code.pro>
2022-10-30 12:55:56 +0300

    Update main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="d-flex justify-center align-center flex-column fill-height">
         <div class="d-flex justify-center text-center pa-16">
-            <h1 class="text-h1">
-                volunteers
+            <h1 class="text-h1 text-lowercase">
+                {{ header }}
             </h1>
 
-            <h2 class="text-h4">
-                CRM
+            <h2 class="text-h4 text-lowercase">
+                {{ subheader }}
             </h2>
         </div>
 
@@ -17,4 +17,12 @@
 </template>
 
 <script lang="ts" setup>
-import TelegramSignInBtn from '@/components/buttons/telegram/sign-in.vue'</script>
+import TelegramSignInBtn from '@/components/buttons/telegram/sign-in.vue'
+
+import { APPLICATION_TITLE } from '@/constants/meta'
+
+import { computed } from 'vue'
+
+const header = computed(() => APPLICATION_TITLE.split(' ')[0])
+const subheader = computed(() => APPLICATION_TITLE.split(' ')[1])
+</script>
