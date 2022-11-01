b6acb549db8692f34b2d37b61807bc5188b9e3a0
Andrey Helldar <helldar@dragon-code.pro>
2022-11-02 02:59:34 +0300

    Fixed storages and roles dashboard stats

--- src/components/charts/storage.vue
+++ src/components/charts/storage.vue
@@ -25,30 +25,10 @@ import { API_DASHBOARD_STORAGES } from '@/constants/api_routes'
 import { computed, onBeforeMount, ref } from 'vue'
 import { collect } from '@/helpers/collection'
 import axios from 'axios'
-import _ from 'lodash'
 
 const items = ref([])
 
 const pluck = (key: string) => collect(items.value).pluck(key).get()
-
-const pluckUsers = () => {
-    let users: any[] = []
-
-    _.each(items.value, (item: any) => {
-        _.each(item.users, (user: any) => {
-            if (! _.includes(users, user.name)) {
-                _.set(users, user.name, {
-                    name: user.name,
-                    data: []
-                })
-            }
-
-            users[user.name].data.push(user.count)
-        })
-    })
-
-    return _.values(users)
-}
 
 const storage = computed(() => {
     return {
@@ -62,7 +42,11 @@ const storage = computed(() => {
             }
         },
 
-        series: pluckUsers()
+        series: [
+            {
+                data: pluck('count')
+            }
+        ]
     }
 })
 
